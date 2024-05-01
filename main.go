682230d233880ebe0312a59d3b81f054278b54a9
Jim Pick <jim@jimpick.com>
2024-05-01 10:48:15 -0700

    Simple http server and Lotus client

--- main.go
+++ main.go
@@ -1,27 +1,44 @@
 package main
 
 import (
-	"context"
+	"errors"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 
 	jsonrpc "github.com/filecoin-project/go-jsonrpc"
 	lotusapi "github.com/filecoin-project/lotus/api"
 )
 
-func main() {
-	url := "https://api.node.glif.io/rpc/v1"
+const url = "https://api.node.glif.io/rpc/v1"
+
+func getAddress(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 
 	var api lotusapi.FullNodeStruct
-	closer, err := jsonrpc.NewMergeClient(context.Background(), url, "Filecoin", []interface{}{&api.Internal, &api.CommonStruct.Internal}, nil)
+	closer, err := jsonrpc.NewMergeClient(ctx, url, "Filecoin",
+		[]interface{}{&api.Internal, &api.CommonStruct.Internal}, nil)
 	if err != nil {
 		log.Fatalf("connecting with lotus failed: %s", err)
 	}
 	defer closer()
 
-	tipset, err := api.ChainHead(context.Background())
+	tipset, err := api.ChainHead(ctx)
 	if err != nil {
 		log.Fatalf("calling chain head: %s", err)
 	}
 	fmt.Printf("Current chain head is: %d %s\n", tipset.Height(), tipset.String())
+	io.WriteString(w, "This is my website!\n")
+}
+
+func main() {
+	http.HandleFunc("/", getAddress)
+
+	err := http.ListenAndServe(":3000", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Println("server closed")
+	} else if err != nil {
+		log.Fatalf("error starting server: %s", err)
+	}
 }
